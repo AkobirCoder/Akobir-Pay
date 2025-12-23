8eda8540435f8130110d64bb7dbe539ac8f76672
AkobirCoder <akobir0308@gmail.com>
2025-12-23 22:34:20 +0500

    ./tailwind.config.js was updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
-            colors: {},
+            colors: {
+                primary: '#00040f',
+                secondary: '#00f6ff',
+                lightWhite: 'rgba(255, 255, 255, 0.7)',
+                lightBlue: 'rgba(9, 151, 124, 0.1)',
+            },
             fontFamily: {},
         },
         screens: {
