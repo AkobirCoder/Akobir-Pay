ecf5d40dd4c49b95f2ce3c380bdf67b28df378d1
AkobirCoder <akobir0308@gmail.com>
2025-12-23 22:14:35 +0500

    ./tailwind.config.js was updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-        colors: {},
-        fontFamily: {},
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {},
+            fontFamily: {},
+        },
+        screens: {
+            xs: '480px',
+            ss: '620px',
+            sm: '768px',
+            md: '1060px',
+            lg: '1200px',
+            xl: '1700px',
+        },
     },
-    screens: {
-        xs: '480px',
-        ss: '620px',
-        sm: '768px',
-        md: '1060px',
-        lg: '1200px',
-        xl: '1700px',
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
 
