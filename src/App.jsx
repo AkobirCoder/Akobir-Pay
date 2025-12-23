73a6f100055c2a4f8334fcf6cb6705537847a079
AkobirCoder <akobir0308@gmail.com>
2025-12-23 22:41:42 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline bg-black text-lightWhite xs:block ss:hidden md:block">
+            <h1 className="text-3xl font-bold underline bg-black font-montserrat text-lightWhite xs:block ss:hidden md:block">
                 Hello world!
             </h1>
         </div>
