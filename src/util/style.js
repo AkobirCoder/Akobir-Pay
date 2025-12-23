f5a6320ec4d5647c8027f6a03d21fcfb3e3a920d
AkobirCoder <akobir0308@gmail.com>
2025-12-23 23:57:29 +0500

    ./src/util/style.js was updated

--- src/util/style.js
+++ src/util/style.js
@@ -7,5 +7,5 @@ export const styles = {
     margin: 'sm:mx-16 mx-6 sm:my-12 my-4',
     container: 'xl:max-w-[1280px] w-full',
     flexCenter: 'flex justify-center items-center',
-    flexStart: 'flex justify-center items-start',
+    flexStart: 'flex justify-start items-center',
 }
