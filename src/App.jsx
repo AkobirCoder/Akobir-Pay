0130029ff439fdeea8c99e6db4eeaaa5b3959289
AkobirCoder <akobir0308@gmail.com>
2025-12-24 00:10:01 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,24 @@ const App = () => {
         paddingY,
         container,
         flexCenter,
-        flexStart
+        flexStart,
+        paragraph,
+        heading,
     } = styles;
 
     return (
         <div className="bg-primary w-full overflow-hidden">
             <div className={`${paddingX} ${paddingY} ${flexCenter}`}>
                 <div className={`${container} ${flexStart}`}>
-                    <p className="text-white">
+                    <div className={`${heading}`}>
                         Navbar
-                    </p>
+                    </div>
                 </div>
             </div>
 
-            <div className={`bg-slate-500 ${paddingX} ${paddingY}`}></div>
+            <div className={`bg-slate-500 ${paddingX} ${paddingY} ${paragraph}`}>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam at aspernatur nemo animi odit harum adipisci libero illum voluptatum eaque non perferendis molestiae assumenda, quod ratione reiciendis. Officiis reprehenderit deserunt nulla hic voluptate repellat quis, enim inventore ex amet itaque, optio quas eos! Id corporis eius atque vitae natus totam.</p>
+            </div>
         </div>
     );
 }
