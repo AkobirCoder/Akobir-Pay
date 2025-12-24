a388837ddad68eef97e84227875a9924a9a7b03b
AkobirCoder <akobir0308@gmail.com>
2025-12-24 23:43:45 +0500

    ./src/util/constants.js was added

---
+++ src/util/constants.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        id: 'home',
+        title: 'Bosh sahifa',
+    },
+    {
+        id: 'services',
+        title: 'Xizmatlar',
+    },
+    {
+        id: 'products',
+        title: 'Mahsulotlar',
+    },
+    {
+        id: 'clients',
+        title: 'Mijozlar',
+    },
+    {
+        id: 'contact',
+        title: 'Aloqa',
+    },
+];
