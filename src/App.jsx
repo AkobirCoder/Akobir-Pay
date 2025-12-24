8fbafb491b73e1bca0d0db46e2ddaae1a9fe7de4
AkobirCoder <akobir0308@gmail.com>
2025-12-24 23:32:12 +0500

    ./src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,15 @@ import { styles } from './util/style';
 import {Navbar} from './components/index';
 
 const App = () => {
-    const {
-        paddingX, 
-        paddingY,
-        container,
-        flexCenter,
-        flexStart,
-        paragraph,
-        heading,
-    } = styles;
+    const {paddingX, container} = styles;
 
     return (
         <div className="bg-primary w-full overflow-hidden">
-            <div className={`${paddingX} ${paddingY} ${flexCenter}`}>
-                <div className={`${container} ${flexStart}`}>
-                    <div className={`${heading}`}>
-                        Navbar
-                    </div>
+            <div className={`${paddingX}`}>
+                <div className={`${container}`}>
+                    <Navbar />
                 </div>
             </div>
-
-            <div className={`bg-slate-500 ${paddingX} ${paddingY} ${paragraph}`}>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam at aspernatur nemo animi odit harum adipisci libero illum voluptatum eaque non perferendis molestiae assumenda, quod ratione reiciendis. Officiis reprehenderit deserunt nulla hic voluptate repellat quis, enim inventore ex amet itaque, optio quas eos! Id corporis eius atque vitae natus totam.</p>
-            </div>
-            <Navbar />
         </div>
     );
 }
