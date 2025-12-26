691adb51e698691262746f823265b4a510681974
AkobirCoder <akobir0308@gmail.com>
2025-12-27 01:05:24 +0500

    ./src/assets/index.js was updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export {default as Logo} from './logo.svg';
 export {default as MenuIcon} from './menu.svg';
 export {default as CloseIcon} from './close.svg';
+export {default as DiscountIcon} from './discount.svg';
