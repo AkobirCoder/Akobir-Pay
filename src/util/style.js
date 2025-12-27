ad9bfe5510faa40a9e9280978f3a646013ec6643
AkobirCoder <akobir0308@gmail.com>
2025-12-27 21:26:49 +0500

    ./src/util/style.js was updated

--- src/util/style.js
+++ src/util/style.js
@@ -10,5 +10,6 @@ export const styles = {
     flexStart: 'flex justify-start items-start',
     flexBetween: 'flex justify-between items-center',
     paragraph: 'font-montserrat font-normal text-lightWhite text-4 text-justify leading-[30.8px]',
-    heading: 'font-montserrat font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-[50%]',
+    headingOne: 'font-montserrat font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]',
+    headingTwo: 'font-montserrat font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-[50%]',
 }
