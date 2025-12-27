94726b197f79f41f046f1a6e99d1eaf3413c295c
AkobirCoder <akobir0308@gmail.com>
2025-12-27 21:46:15 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { styles } from './util/style';
 import {Home, Navbar} from './components/index';
 
 const App = () => {
-    const {paddingX, container} = styles;
-
     return (
         <div className="bg-primary w-full overflow-hidden">
-            <div className={`${paddingX}`}>
-                <div className={`${container}`}>
+            <div className={`${styles.paddingX} ${styles.flexCenter} border-b-2 border-b-slate-700`}>
+                <div className={`${styles.container}`}>
                     <Navbar />
+                </div>
+            </div>
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.container}`}>
                     <Home />
                 </div>
             </div>
