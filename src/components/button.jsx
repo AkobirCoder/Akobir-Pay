40f4df94f0069e67123ef357b48e522a64fd98c7
AkobirCoder <akobir0308@gmail.com>
2025-12-28 00:05:08 +0500

    ./src/components/button.jsx was added

---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button type='button' className={'py-4 px-6 font-montserrat font-medium text-[18px] rounded-md outline-none bg-blue-gradient'}>
+            Boshlash
+        </button>
+    );
+}
+
+export default Button;
