d081b36d7768498e8639bd1a9bd1792cbbe61ca3
AkobirCoder <akobir0308@gmail.com>
2025-12-27 23:49:06 +0500

    ./src/components/home.jsx was updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { styles } from '../util/style';
-import { DiscountIcon } from '../assets';
+import { DiscountIcon, HomeRobotImage } from '../assets';
 
 const Home = () => {
     return (
-        <section id='home' className={`md:flex-row flex flex-col ${styles.paddingY}`}>
+        <section id='home' className={`md:flex-row flex flex-col items-center ${styles.paddingY}`}>
+            {/* --- Home section animation image --- */}
+                <div className={`flex-1 ${styles.flexStart} md:my-0 my-10`}>
+                    <img src={HomeRobotImage} alt="Home section Robot" className='w-[100%] h-[100%] relative z-10 rounded-2xl' />
+                </div>
+            {/* --- Home section animation image --- */}
+
             {/* --- Home section information --- */}
-                <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+                <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 relative`}>
                     {/* --- Home section discount information --- */}
                         <div 
                             className={'flex flex-row items-center gap-1 py-[6px] px-4 rounded-[10px] mb-2 bg-lightWhite bg-discount-gradient'}
@@ -35,12 +41,13 @@ const Home = () => {
                         </div>
                     {/* --- Home section description --- */}
 
+                    {/* Home section gradient background */}
+                        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink-gradient' />
+                        <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white-gradient' />
+                        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue-gradient' />
+                    {/* Home section gradient background */}
                 </div>
             {/* --- Home section information --- */}
-
-            {/* --- Home section animation image --- */}
-                <div className=''></div>
-            {/* --- Home section animation image --- */}
         </section>
     );
 }
