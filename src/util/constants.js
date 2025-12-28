0dbe2cbda274780bddb6392d108bddf3c8226ee1
AkobirCoder <akobir0308@gmail.com>
2025-12-28 22:41:28 +0500

    ./src/util/constants.js was updated

--- src/util/constants.js
+++ src/util/constants.js
@@ -61,3 +61,24 @@ export const services = [
         content: "Balansni o'tkazish kredit kartasi sizga foizlarni to'lashda ko'p pul tejash imkonini beradi.",
     }
 ];
+
+export const feedbacks = [
+    {
+        id: 1,
+        content: 'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.',
+        name: 'Herman Jenson',
+        title: 'Founder & Leader',
+    },
+    {
+        id: 2,
+        content: "Money makes your life easier. If you're lucky to have it, you're lucky.",
+        name: 'Steve Mark',
+        title: 'Founder & Leader',
+    },
+    {
+        id: 3,
+        content: 'It is usually people in the money business, finance and international trade that are really rich.',
+        name: 'Kenn Gallagher',
+        title: 'Founder & Leader',
+    }
+];
