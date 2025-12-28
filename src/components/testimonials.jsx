201fc031f1aa120b644d12584d4be161d7b13612
AkobirCoder <akobir0308@gmail.com>
2025-12-28 22:41:07 +0500

    ./src/components/testimonials.jsx was updated

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { styles } from '../util/style';
+import { feedbacks } from '../util/constants';
+import FeedbackCard from './feedback-card';
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <div className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+            <div className={'absolute z-0 w-[60%] h-[60%] -right-[50%] rounded-full blue-gradient bottom-40'} />
+
+            <div 
+                className={`
+                    w-full 
+                    ${styles.flexBetween} 
+                    md:flex-row flex-col 
+                    sm:mb-16 mb-6 
+                    relative 
+                    z-[1]
+                `}
+            >
+                <h2 className={`${styles.headingTwo}`}>
+                    Odamlar biz haqimizda qanday fikrda?
+                </h2>
+                <div className={'w-full md:mt-0 mt-6'}>
+                    <p className={`${styles.paragraph} text-left max-w-[550px]`}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Esse quos commodi vero quod maxime tenetur totam saepe! Earum, deserunt soluta!
+                    </p>
+                </div>
+            </div>
+
+            <div className={'flex flex-wrap sm:justify-start justify-center w-full relative z-[1]'}>
+                {
+                    feedbacks.map((feedback, index) => {
+                        return (
+                            <FeedbackCard key={feedback.id} {...feedback} />
+                        );
+                    })
+                }
+            </div>
+        </div>
     );
 }
 
