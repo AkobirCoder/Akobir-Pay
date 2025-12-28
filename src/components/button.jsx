00a609eddfa13f94b9b877cbe300dc19548a7469
AkobirCoder <akobir0308@gmail.com>
2025-12-28 12:13:59 +0500

    ./src/components/button.jsx was updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({styles}) => {
     return (
-        <button type='button' className={'py-4 px-6 font-montserrat font-medium text-[18px] rounded-md outline-none bg-blue-gradient'}>
+        <button 
+            type='button' 
+            className={`py-4 px-6 font-montserrat font-medium text-[18px] rounded-xl outline-none ${styles} bg-blue-gradient`}
+        >
             Boshlash
         </button>
     );
