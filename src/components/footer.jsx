072f9aaafc5803d70bdd9555d875af7b15af318f
AkobirCoder <akobir0308@gmail.com>
2025-12-29 23:56:10 +0500

    ./src/components/footer.jsx was updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { styles } from '../util/style';
 import { Logo } from '../assets';
-import { footerLinks } from '../util/constants';
+import { footerLinks, socialMedia } from '../util/constants';
 import FooterLinkCard from './footer-link-card';
 
 const Footer = () => {
@@ -28,11 +28,33 @@ const Footer = () => {
 
             <div className={`w-full ${styles.flexBetween} md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]`}>
                 <p className={'font-montserrat font-normal text-center text-[18px] leading-[27px] text-white capitalize'}>
-                    Copyright 2025 AkobirPay. All right reserved
+                    Copyright © 2025 AkobirPay. All right reserved
                 </p>
 
                 <div className={'flex flex-row md:mt-0 mt-6'}>
-                    
+                    {
+                        socialMedia.map((social, index) => {
+                            return (
+                                <a 
+                                    href={social.link} 
+                                    target='_blank' 
+                                    rel='noreferrer'
+                                    className={`${index === socialMedia.length - 1 ? 'mr-0' : 'mr-6'}`}
+                                >
+                                    <img 
+                                        src={social.icon} 
+                                        key={social.id} 
+                                        alt={social.id} 
+                                        className={`
+                                            w-[20px] h-[20px] 
+                                            object-contain 
+                                            cursor-pointer
+                                        `}
+                                    />
+                                </a>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
