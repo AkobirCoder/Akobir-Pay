3535f389a31eb280f4683ff3cd0debef69eba1e8
AkobirCoder <akobir0308@gmail.com>
2025-12-29 15:13:48 +0500

    ./src/components/clients.jsx was updated

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { styles } from '../util/style';
+import { clients } from '../util/constants';
+import ClientCard from './client-card';
 
 const Clients = () => {
     return (
-        <div className={`${styles.flexCenter} my-4`}>Clients</div>
+        <div className={`${styles.flexCenter} my-4`}>
+            <div className={`${styles.flexCenter} flex-wrap w-full`}>
+                {
+                    clients.map((client, index) => {
+                        return (
+                            <ClientCard key={client.id} logo={client.logo} index={index} />
+                        );
+                    })
+                }
+            </div>
+        </div>
     );
 }
 
